cc9a99c8408ff962086e34c2b902ff6eee86c4c5
abreaux26 <angelbreaux26@gmail.com>
2021-03-05 20:31:24 -0600

    Add validations to bulk discounts model

--- app/models/bulk_discount.rb
+++ app/models/bulk_discount.rb
@@ -1,3 +1,6 @@
 class BulkDiscount < ApplicationRecord
+  validates_presence_of :percentage_discount, :quantity_threshold
+  validates_numericality_of :quantity_threshold
+
   belongs_to :merchant
 end
