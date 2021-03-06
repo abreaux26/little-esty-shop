e4b63012a987ed191e8c72251442dab18b519780
abreaux26 <angelbreaux26@gmail.com>
2021-03-05 20:30:22 -0600

    Create spec for bulk discount validations

--- spec/models/bulk_discount_spec.rb
+++ spec/models/bulk_discount_spec.rb
@@ -4,4 +4,10 @@ RSpec.describe BulkDiscount, type: :model do
   describe 'relationhips' do
     it { should belong_to :merchant }
   end
+
+  describe 'validations' do
+    it { should validate_presence_of :percentage_discount }
+    it { should validate_presence_of :quantity_threshold }
+    it { should validate_numericality_of :quantity_threshold }
+  end
 end
